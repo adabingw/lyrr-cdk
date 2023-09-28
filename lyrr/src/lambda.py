59bf3bc062a09af20c40153771e7623cbf2f1d7c
Ada <adabingw@yahoo.com>
2023-09-27 22:01:04 -0400

    :sake:

--- lyrr/src/lambda.py
+++ lyrr/src/lambda.py
@@ -1,12 +1,20 @@
 from data import collect, get_artist
 from model import generator, get_model
 from constants import get_hf_write, get_hf_read
+import json
 
 def handler(event, context):
     print(event)
-    path = event['path']
+    path = ""
+    try:
+        path = event['path']
+    except: 
+        return {
+            'statusCode': 200,
+            'body': str(event)
+        }
         
-    if path == '/artist':
+    if path == '/lyrr-backend/artist':
         print("getting artist")
         if event['queryStringParameters'] is None:
             return {
@@ -19,10 +27,14 @@ def handler(event, context):
         
         return {
             'statusCode': 200,
-            'body': artists
+            # 'headers': {
+            #     "Access-Control-Allow-Origin": "*", # // Required for CORS support to work
+            #     "Access-Control-Allow-Credentials": True #, // Required for cookies, authorization headers with HTTPS
+            # },
+            'body': str(artists)
         }
         
-    if path == '/lyrics':
+    if path == '/lyrr-backend/lyrics':
         print("getting lyrics")
         if event['queryStringParameters'] is None: 
             return {
@@ -38,10 +50,14 @@ def handler(event, context):
         generated_lyrics = generator(text=lyrics, name=artist)
         return {
             'statusCode': 200,
+            # 'headers': {
+            #     "Access-Control-Allow-Origin": "*", # // Required for CORS support to work
+            #     "Access-Control-Allow-Credentials": True #, // Required for cookies, authorization headers with HTTPS
+            # },
             'body': generated_lyrics
         }
     
-    if path == '/train':
+    if path == '/lyrr-backend/train':
         print("training model")
         if event['queryStringParameters'] is None: 
             return {
@@ -53,17 +69,21 @@ def handler(event, context):
             get_model(artist)
             return {
                 'statusCode': 200,
+                # 'headers': {
+                #     "Access-Control-Allow-Origin": "*", # // Required for CORS support to work
+                #     "Access-Control-Allow-Credentials": True #, // Required for cookies, authorization headers with HTTPS
+                # },
                 'body': event['path']
             }
         except: 
             return {
                 'statusCode': 500,
-                'body': event['path']
+                'body': str(event)
             }
         
     
     return {
         'statusCode': 500,
-        'body': event['path']
+        'body': str(event)
     }
 
